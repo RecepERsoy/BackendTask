551699c1b02034941e3d824140015470492830f0
Recep Ersoy <ersoyrecep7@gmail.com>
2026-03-16 18:19:04 +0300

    Auth mikroservisi için Onion mimari katmanları oluşturuldu, User entity ve AuthDbContext eklendi.

---
+++ Auth.Infrastructure/Context/AuthDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Auth.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Auth.Infrastructure.Context
+{
+    public class AuthDbContext : DbContext
+    {
+        public AuthDbContext(DbContextOptions<AuthDbContext> options) : base(options)
+        {
+        }
+
+        
+        public DbSet<User> Users { get; set; }
+    }
+}
